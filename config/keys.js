029260faa9d3332c183660ba5f2ce2d2cc5adb04
tuncozturk <tuncozturk@users.noreply.github.com>
2018-07-29 17:42:14 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = { 
-    mongoURI: 'mongodb://devconnector:elma1976@ds259111.mlab.com:59111/devconnex'
+    mongoURI: 'mongodb://devconnector:******@ds259111.mlab.com:59111/devconnex'
 
 }
